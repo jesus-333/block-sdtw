c66c67a5dc7564f9609e1174ea88a66eed2ac371
jesus-333 <albi25.zanca@gmail.com>
2024-11-16 19:30:43 +0100

    Updates

--- computation_plot.py
+++ computation_plot.py
@@ -13,7 +13,7 @@ import numpy as np
 
 path_files = "Results/computation_time/raspberry/"
 
-t_list = (np.arange(20) + 2) * 50
+t_list = (np.arange(19) + 2) * 50
 
 loss_type_list = [1, 3]
 
@@ -26,6 +26,17 @@ plot_config = dict(
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+label_list = []
+results_matrix = []
 
-# loss_type_str = 'SDTW_rust' if loss_type_to_use == 0 else 'SDTW_standard' if loss_type_to_use == 1 else 'SDTW_divergence' if loss_type_to_use == 2 else 'SDTW_block' if loss_type_to_use == 3 else 'SDTW_block_divergence'
+for i in range(len(loss_type_list)) : # Iterate over loss function variation
+    loss_type = loss_type_list[i]
+    loss_type_str = 'SDTW_rust' if loss_type == 0 else 'SDTW_standard' if loss_type == 1 else 'SDTW_divergence' if loss_type == 2 else 'SDTW_block' if loss_type == 3 else 'SDTW_block_divergence'
+
+    label_list.append(loss_type_str)
+
+    for j in range(len(t_list)) : # Iterate over sequence length
+        t = t_list[j]
+
+
 
