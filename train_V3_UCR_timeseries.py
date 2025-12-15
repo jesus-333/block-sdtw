0eff0b4f77860972e63d0fb8dd8578a8b42a9ac0
Jesus HPC_lux <alberto.zancanaro.1@gmail.com>
2025-12-15 17:07:48 +0000

    Update train script V3

--- train_V3_UCR_timeseries.py
+++ train_V3_UCR_timeseries.py
@@ -29,7 +29,7 @@ config = dict(
     # Training parameters
     use_z_score_normalization = True,    # If True a z-score normalization will be applied signal by signal within each dataset
     portion_of_signals_for_input = 0.85, # Portion of the signals to use for training (the rest will be used for prediction)
-    n_samples_to_predict = -1,            # Number of samples to predict (If negative it is ignored and the portion_of_signals_for_input is used to define the number of samples to predict. Otherwise, this parameter override portion_of_signals_for_input)
+    n_samples_to_predict = 100,            # Number of samples to predict (If negative it is ignored and the portion_of_signals_for_input is used to define the number of samples to predict. Otherwise, this parameter override portion_of_signals_for_input)
     batch_size = -1,                     # Batch size for training
     lr = 0.001,                          # Learning rate (lr)
     max_epochs = 60,                         # Number of epochs to train the model
@@ -42,7 +42,7 @@ config = dict(
     alpha = 1,                           # Multiplier of the reconstruction error
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Soft-DTW/block SDTW config
-    block_size = 50,
+    block_size = 10,
     edge_samples_ignored = 0,            # Ignore this number of samples during the computation of the reconstructation loss
     gamma_dtw = 1,                       # Hyperparameter of the SDTW. Control the steepness of the soft-min inside the SDTW. The closer to 0 the closer the soft-min approximate the real min
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -61,6 +61,7 @@ config = dict(
 
 loss_function_to_use_list = ['MSE', 'SDTW', 'SDTW_divergence', 'pruned_SDTW', 'OTW', 'block_SDTW', 'block_SDTW_divergence']
 loss_function_to_use_list = ['block_SDTW', 'block_SDTW_divergence']
+# loss_function_to_use_list = ['block_SDTW_divergence']
 
 n_neurons = 256  # Number of neurons in the hidden layers
 
@@ -130,7 +131,7 @@ for i in range(len(list_all_dataset_name)):
 
     if length_signal_to_predict <= config['block_size'] :
         print(f"Skipping dataset {name_dataset} because the portion of the signal to predict ({length_signal_to_predict}) is too small compared to the block size ({config['block_size']})")
-        print("fThe length of the signal to predict should be at least twice the block size (i.e. block_size/length_signal_to_predict <= 0.5)")
+        print("The length of the signal to predict should be at least twice the block size (i.e. block_size/length_signal_to_predict <= 0.5)")
         print(f"Current Ratio: {config['block_size'] / length_signal_to_predict:.3f} > 0.5")
         continue
 
@@ -197,8 +198,14 @@ for i in range(len(list_all_dataset_name)):
 
         model = MultiLayerPerceptron(layers, loss_function, config)
         model.save_model_path = save_model_path_for_current_dataset
+        
+        if loss_function_to_use == 'block_SDTW' :
+            model_name = f'model_block_SDTW_{config["block_size"]}'
+        elif loss_function_to_use == 'block_SDTW_divergence' :
+            model_name = f'model_block_SDTW_divergence_{config["block_size"]}'
+        else :
+            model_name = f'model_{loss_function_to_use}'
 
-        model_name = f'model_block_SDTW_{config["block_size"]}' if loss_function_to_use == 'block_SDTW' else f'model_{loss_function_to_use}'
         model.model_name = f"{model_name}"
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
